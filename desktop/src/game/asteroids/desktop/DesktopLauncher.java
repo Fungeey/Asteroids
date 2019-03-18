d94464f13515fe7492838948c054560b507a5d42
lightningstudios <ethan.wong@hotmail.com>
2019-03-18 11:16:48 -0400

    Initial commit

---
+++ desktop/src/game/asteroids/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.asteroids.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.asteroids.Asteroids;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Asteroids(), config);
+	}
+}
