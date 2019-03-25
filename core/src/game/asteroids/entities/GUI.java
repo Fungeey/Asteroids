1e271b6fd2a90d486e2a2bfc22f4c52b7521c4e4
Aaron <Aaron@ABCRLEE-ALPC>
2019-03-25 14:12:06 -0400

    Added GUI system and a test font

--- core/src/game/asteroids/entities/GUI.java
+++ core/src/game/asteroids/entities/GUI.java
@@ -11,21 +11,11 @@ public class GUI {
 
 	public static void draw(SpriteBatch batch){
 		textLayout.setText(font, "Hello");
-		// gdx.graphics.getwidth() is not accurate
-		//
 		drawText(batch, textLayout, Gdx.graphics.getWidth()/2, 0);
 	}
 
 	private static void drawText(SpriteBatch batch, GlyphLayout text, float x, float y){
-		drawText(batch, text, x, y, TextAlign.CENTER);
-	}
-
-	private static void drawText(SpriteBatch batch, GlyphLayout text, float x, float y, TextAlign align){
 		font.draw(batch, text, x, y);
-	}
-
-	private static enum TextAlign{
-		TOPLEFT, TOPRIGHT, BOTTOMLEFT, BOTTOMRIGHT, CENTER
 	}
 
 	public static void dispose(){
