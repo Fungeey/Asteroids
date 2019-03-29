0a2cd3bf73b8b8d74d29e3608d67ea0f563ff9e0
lightningstudios126 <ethan.wong@hotmail.com>
2019-03-28 23:50:47 -0400

    added collisions for saucers, asteroids, and saucer bullets

--- core/src/game/asteroids/entities/CollisionHandler.java
+++ core/src/game/asteroids/entities/CollisionHandler.java
@@ -6,23 +6,22 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
 /**
- * Helper class that defines what happens when two Entities collide based on their class type
+ * Helper class that defines what happens when two Entities collide based on their collision mask settings
  */
 public class CollisionHandler implements ContactListener {
 	//<editor-fold desc="LAYER MASK SETUP">
 	static final short LAYER_DEFAULT = 0x0001;
-	static final short LAYER_ASTEROIDS = 0x002;
+	static final short LAYER_ASTEROIDS = 0x0002;
 	static final short LAYER_PLAYER = 0x0004;
 	static final short LAYER_PLAYER_BULLET = 0x0008;
-	static final short LAYER_SIGNAL = 0x10;
 	static final short LAYER_SAUCER = 0x0010;
 	static final short LAYER_SAUCER_BULLET = 0x0020;
 
 	private static final short MASK_DEFAULT = -1;
-	private static final short MASK_ASTEROIDS = LAYER_PLAYER | LAYER_PLAYER_BULLET;
-	private static final short MASK_PLAYER = LAYER_ASTEROIDS;
+	private static final short MASK_ASTEROIDS = LAYER_PLAYER | LAYER_SAUCER | LAYER_PLAYER_BULLET | LAYER_SAUCER_BULLET;
+	private static final short MASK_PLAYER = LAYER_ASTEROIDS | LAYER_SAUCER | LAYER_SAUCER_BULLET;
 	private static final short MASK_PLAYER_BULLET = (LAYER_ASTEROIDS | LAYER_SAUCER);
-	private static final short MASK_SAUCER = (LAYER_ASTEROIDS | LAYER_PLAYER | LAYER_PLAYER_BULLET);
+	private static final short MASK_SAUCER = LAYER_ASTEROIDS | LAYER_PLAYER | LAYER_PLAYER_BULLET;
 	private static final short MASK_SAUCER_BULLET = (LAYER_ASTEROIDS | LAYER_PLAYER);
 	//</editor-fold>
 
@@ -38,29 +37,48 @@ public class CollisionHandler implements ContactListener {
 	public void beginContact(Contact contact){
 		Entity a = (Entity) contact.getFixtureA().getBody().getUserData();
 		Entity b = (Entity) contact.getFixtureB().getBody().getUserData();
-
-		if ((a.layer | b.layer) == (LAYER_ASTEROIDS | LAYER_PLAYER)) {
+		
+		int combined = a.layer | b.layer;
+		
+		if (combined == (LAYER_ASTEROIDS | LAYER_PLAYER)) {
 			if (a.layer == LAYER_ASTEROIDS) {
-				Player.score += ((Asteroid)a).getPointValue();
+				Player.score += ((Destructable)a).getPointValue();
 				a.delete();
 				((Player)b).loseLife();
 			} else {
-				Player.score += ((Asteroid)b).getPointValue();
+				Player.score += ((Destructable)b).getPointValue();
 				b.delete();
 				((Player)a).loseLife();
 			}
-		} else if ((a.layer | b.layer) == (LAYER_ASTEROIDS | LAYER_PLAYER_BULLET)) {
+		} else if (combined == (LAYER_ASTEROIDS | LAYER_PLAYER_BULLET)) {
 			if (a.layer == LAYER_ASTEROIDS) Player.score += ((Destructable) a).getPointValue();
-			else Player.score += ((Asteroid)b).getPointValue();
+			else Player.score += ((Destructable)b).getPointValue();
 			a.delete();
 			b.delete();
-		}else if((a.layer | b.layer) == (LAYER_SAUCER | LAYER_PLAYER_BULLET)){
-
+		}else if(combined == (LAYER_SAUCER | LAYER_PLAYER_BULLET)){
 			if (a.layer == LAYER_SAUCER) Player.score += ((Destructable) a).getPointValue();
 			else Player.score += ((Destructable)b).getPointValue();
 			a.delete();
 			b.delete();
-		}
+		} else if (combined == (LAYER_PLAYER | LAYER_SAUCER_BULLET)) {
+		    if (a.layer == LAYER_PLAYER) {
+                ((Player) a).loseLife();
+                b.delete();
+            } else {
+                ((Player) b).loseLife();
+                a.delete();
+            }
+        } else if (combined == (LAYER_PLAYER | LAYER_SAUCER)) {
+		    if (a.layer == LAYER_PLAYER) {
+                ((Player) a).loseLife();
+            } else ((Player) b).loseLife();
+        } else if (combined == (LAYER_SAUCER | LAYER_ASTEROIDS)) {
+		    a.delete();
+		    b.delete();
+        } else if (combined == (LAYER_ASTEROIDS | LAYER_SAUCER_BULLET)) {
+		    a.delete();
+		    b.delete();
+        }
 	}
 
 	public void endContact(Contact contact) {}
